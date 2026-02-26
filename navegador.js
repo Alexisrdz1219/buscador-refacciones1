a0149ec8d4c96c1d562968681a49a83cab0b25aa
Alexis Rodriguez <ealexisrdz19@gmail.com>
2026-02-26 10:27:30 -0600

    Sxxz

--- navegador.js
+++ navegador.js
@@ -185,8 +185,7 @@ contenedorTags.querySelectorAll(".badge").forEach(t => t.remove());
       modeloSeleccionado = maquinamod; // 🔥 guardamos el modelo
 
       console.log("BUSCANDO POR MODELO:", maquinamod);
-      console.log("CLICK EN:", e.currentTarget);
-console.log("MODELO:", e.currentTarget.dataset.maquinamod);
+      console.log("ID QUE ENVÍO AL BACK:", id);
 
       const res = await fetch(
         `${API}/refacciones-por-maquinamod?maquinamod=${encodeURIComponent(maquinamod)}`
