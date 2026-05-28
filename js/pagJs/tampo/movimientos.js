5356cee0e1401f45369ce5e91dbe7948d4f4af3c
Alexis Rodriguez <ealexisrdz19@gmail.com>
2026-05-28 15:29:46 -0600

    Movimientos

---
+++ js/pagJs/tampo/movimientos.js
@@ -0,0 +1,82 @@
+const API =
+"https://buscador-refaccionesbackend.onrender.com";
+
+async function cargarMovimientos(){
+
+    try{
+
+        const res = await fetch(
+
+            `${API}/movimientos`
+
+        );
+
+        const data = await res.json();
+
+        console.log(data);
+
+        const tbody =
+        document.getElementById("tbodyMovimientos");
+
+        tbody.innerHTML = "";
+
+        data.forEach((mov) => {
+
+            tbody.innerHTML += `
+
+                <tr>
+
+                    <td>
+
+                        ${new Date(mov.fecha)
+                        .toLocaleString()}
+
+                    </td>
+
+                    <td>
+
+                        ${mov.refinterna || ""}
+
+                    </td>
+
+                    <td>
+
+                        ${mov.nombreprod || ""}
+
+                    </td>
+
+                    <td>
+
+                        ${mov.cantidad}
+
+                    </td>
+
+                    <td>
+
+                        ${mov.solicitado_por || ""}
+
+                    </td>
+
+                    <td>
+
+                        ${mov.entregado_por || ""}
+
+                    </td>
+
+                </tr>
+
+            `;
+
+        });
+
+    }catch(error){
+
+        console.log(error);
+
+        alert("Error al cargar");
+
+    }
+
+}
+
+cargarMovimientos();
